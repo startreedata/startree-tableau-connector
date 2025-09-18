c73a229675c498225771539e81c0d8502ccdd2d0
m0g0xxf <madhulika.ghosh@walmart.com>
2025-09-18 23:28:27 +0530

    Externalised multistage option to set externally

--- plugins/pinotjdbc/connectionBuilder.js
+++ plugins/pinotjdbc/connectionBuilder.js
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 (function dsbuilder(attr) {
-    const urlBuilder = "jdbc:pinot://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort];
+    const urlBuilder = "jdbc:pinot://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "?enableNullHandling=true&useMultiStageEngine="+attr["v-multistage"];
 
     return [urlBuilder];
 })
